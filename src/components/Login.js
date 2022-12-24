2e5f3169be8c685a85ee15386abb70c428189177
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2022-12-24 13:00:18 +0530

    minor improvement

--- src/components/Login.js
+++ src/components/Login.js
@@ -35,7 +35,6 @@ const Login = () => {
     const response = await fetch(
       "https://chrome-note-backend-vercel.vercel.app/api/auth/login",
       {
-        mode: "no-cors",
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Grid, Item, Button, Container } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import NavContext from "../context/navbar/NavContext";
 
 const Logout = () => {
+  const context = useContext(NavContext);
+  const { setValue } = context;
+  const handleChange = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+    setValue("/");
+  };
+  let navigate = useNavigate();
   return (
     <div>
       <Container maxWidth="sm">
@@ -16,7 +26,12 @@ const Logout = () => {
             <h4>Are You Sure You Want To Logout ?</h4>
           </Grid>
           <Grid item xs={6}>
-            <Button variant="contained" sx={{ width: 150 }} color="success">
+            <Button
+              variant="contained"
+              sx={{ width: 150 }}
+              color="success"
+              onClick={handleChange}
+            >
               Yes
             </Button>
           </Grid>
